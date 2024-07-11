7352a818c080e08451c9b1a8611f96dad855842f
maxingyuan <mxydl2009@163.com>
2024-07-11 19:43:57 +0800

    feat: 完善postversion脚本

--- scripts/post-version.js
+++ scripts/post-version.js
@@ -5,10 +5,44 @@
 const { spawn } = require('child_process');
 const simpleGit = require('simple-git');
 const git = simpleGit();
+const npmLog = require('npmlog');
 
 async function postVersion() {
   const status = await git.status();
-  console.log(status);
+  if (status.modified[0] !== 'package.json') {
+    console.error('modified files not only package.json!');
+    process.exit(1);
+  }
+  await git.add('.');
+
+  // 提交更改
+  const message = 'ci: bump version';
+  await git.commit(message);
+
+  // 推送到远程仓库
+  await git.push('origin', 'main');
+
+  // 推送标签到远程仓库
+  await git.pushTags('origin');
+
+  const publishment = spawn('npm', ['publish'], {
+    cwd: process.cwd(),
+    stdio: 'inherit',
+  });
+  process.on('error', (e) => {
+    npmLog.error('npm publish failed!');
+    npmLog.error(e);
+    process.exit(1);
+  });
+  // 使用stdio: inherit后，子进程调用error来监听error事件
+  publishment.on('error', (e) => {
+    npmLog.error('npm publish failed!');
+    npmLog.error(e);
+    process.exit(1);
+  });
+  publishment.on('exit', () => {
+    npmLog.success('npm publish successfully!');
+  });
 }
 
 postVersion();
